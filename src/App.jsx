134cfe1c9ab680e39ffa48b2a0375e0c01b57ba9
rfumic <rokofumic@gmail.com>
2023-05-22 17:32:50 +0200

    Added images

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,21 @@ function App() {
       description: 'a forum for students built with vue.js and firebase',
       repoUrl: 'https://github.com/rfumic/studiu',
       appUrl: 'https://frabjous-licorice-afe890.netlify.app/',
-      image:
-        'https://raw.githubusercontent.com/rfumic/studiu/master/screenshots/post.png',
+      image: 'src/assets/studiu.png',
     },
     {
       title: 'sudoku race',
       description: 'fullstack online sudoku made with vue.js and express',
       repoUrl: 'https://github.com/rfumic/sudoku-race-frontend',
       appUrl: 'https://sudoku-race.surge.sh/',
-      image:
-        'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.cameraegg.org%2Fwp-content%2Fuploads%2F2016%2F01%2FNikon-D500-Sample-Images-2.jpg&f=1&nofb=1&ipt=ce6d5afe4aa0120fb6c60e38296cda216c3b5337dc4b5eba7b347f2f914abd45&ipo=images',
+      image: 'src/assets/sudoku-race.jpg',
     },
     {
       title: 'remismd',
       description: 'a markdown editor built with vue.js and electron',
       repoUrl: 'https://github.com/rfumic/RemisMD',
       appUrl: 'https://github.com/rfumic/RemisMD/releases',
+      image: 'src/assets/remis.jpg',
     },
     ,
   ];
